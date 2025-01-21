e6fd9c0b4711e61e6683fe505089616f4ec18821
Sean Omeife <omeifesean@gmail.com>
2025-01-21 18:06:00 +0000

    Completed
    Completed 3 exercises and study drills

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:") #This line prints text output
+
+print("\nHens", 25.0 + 30.0 / 6.0) # This line prints text and arithmetic output
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("\nNow I will count the eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7) # This line performs comparative arithmetic
+
+print("\nWhat is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print ("\nHow about some more?")
+
+print("\nIs it greater?", 5 > -2)
+print("Is it greater or equal?",5 >= -2)
+print("Is it less or equal?", 5 <= -2)
