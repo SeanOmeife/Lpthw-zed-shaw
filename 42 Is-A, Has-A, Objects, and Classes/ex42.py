5f8931b8c0e56c15c2f4b7bd0511a8b0b864b0dc
Sean Omeife <omeifesean@gmail.com>
2025-07-04 13:31:17 +0100

    Completed exercise 42 solution

--- 42 Is-A, Has-A, Objects, and Classes/ex42.py
+++ 42 Is-A, Has-A, Objects, and Classes/ex42.py
@@ -24,14 +24,15 @@ class Person(object):
         self.name = name
         
         ## Person has-a pet of some kind
-        self.pet = None
+        self.pet = None 
+    
 
 ## ?
 class Employee(Person):
     
     def __init__(self, name, salary):
         ## ?? hmm what is this strange magic?
-        super(Employee, self).__init__(name)
+        super(Employee, self).__init__(name) 
         ## ??
         self.salary = salary
         
